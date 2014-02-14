66c5bb8f161f47877eeaff29c2e1ee8e9a90978e
Jesse Baird <jebaird@gmail.com>
2014-02-14 10:44:35 -0600

    added tasks for build process takes gadget dirs -> .gadget files with grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,8 +19,83 @@ grunt.initConfig({
         // used for developing without visual studio
         // text widget
         text: {
-            src: 'src/',
-            dest: 'dest/'
+            src: 'cctext.gadget/',
+            dest: 'appdata'
+        }
+    },
+
+    compress: {
+
+        //text gadget
+        text: {
+            options: {
+                mode: 'zip',
+                archive: 'build/cctext.zip'
+            },
+            files: [
+                {
+                     cwd: 'cctext.gadget/',
+                    src: ['**'],
+                    expand: true
+                }
+            ]
+        },
+        weather: {
+            options: {
+                mode: 'zip',
+                archive: 'build/ccweather.zip'
+            },
+            files: [
+                {
+                     cwd: 'ccweather.gadget/',
+                    src: ['**'],
+                    expand: true
+                }
+            ]
+        },
+        pidglet: {
+            options: {
+                mode: 'zip',
+                archive: 'build/ccpidglet.zip'
+            },
+            files: [
+                {
+                     cwd: 'ccpidglet.gadget/',
+                    src: ['**'],
+                    expand: true
+                }
+            ]
+        },
+        date: {
+            options: {
+                mode: 'zip',
+                archive: 'build/ccdate.zip'
+            },
+            files: [
+                {
+                     cwd: 'ccdate.gadget/',
+                    src: ['**'],
+                    expand: true
+                }
+            ]
+        }
+    },
+    rename: {
+        text: {
+            src: 'build/cctext.zip',
+            dest: 'build/cctext.gadget'
+        },
+        weather: {
+            src: 'build/ccweather.zip',
+            dest: 'build/ccweather.gadget'
+        },
+        pidglet: {
+            src: 'build/ccpidglet.zip',
+            dest: 'build/ccpidglet.gadget'
+        },
+        date: {
+            src: 'build/ccdate.zip',
+            dest: 'build/ccdate.gadget'
         }
     }
 });
@@ -28,8 +103,11 @@ grunt.initConfig({
 // Load the plugin that provides the "uglify" task.
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-contrib-copy');
+grunt.loadNpmTasks('grunt-contrib-compress');
+grunt.loadNpmTasks('grunt-rename');
 
 // Default task(s).
 grunt.registerTask('default', ['watch']);
+grunt.registerTask('build', ['compress','rename']);
 
 };
